bc6c75289bb6fc4d7e669c8cfe6eaaaf87d68c4f
karishmabisht14 <karishma.qt14@gmail.com>
2023-05-06 18:55:05 +0530

    Build the main page using react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HeaderC from './components/header/HeaderC';
+import Section from './components/layout/Section';
+import FooterC from './components/footer/FooterC';
+
+function App() {
+  return (
+    <React.Fragment>
+        <HeaderC />
+        <Section />
+        <FooterC/>
+    </React.Fragment>
+  );
+}
+
+export default App;
