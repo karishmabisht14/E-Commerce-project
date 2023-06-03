fdf8495d405a80e7fcf3ffb04dbec0fc9d88f589
karishmabisht14 <karishma.qt14@gmail.com>
2023-06-03 15:03:26 +0530

    create Cart Component

--- src/components/header/HeaderCartButton.js
+++ src/components/header/HeaderCartButton.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./HeaderCartButton.css";
 import { Button } from "react-bootstrap";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = props => {
     return (
         <React.Fragment>
-            <Button className="button" variant="outline-info">cart</Button>
-            <h3 className="badge badge-wrap">0</h3>
+            <Button className="button" variant="outline-info" onClick={props.onClick}>cart</Button>
+            <h3 variant = "outline-info" className="badge badge-wrap">0</h3>
         </React.Fragment>            
     );
 };
